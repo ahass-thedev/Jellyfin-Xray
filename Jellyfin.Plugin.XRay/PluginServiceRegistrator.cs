58c3496b2f26106d7253c36a46e456df77f9e4b2
Ahmed H <achmadoo@outlook.com>
2026-03-16 22:24:23 -0400

    Fix RegisterServices signature - IServerApplicationHost in 10.11

--- Jellyfin.Plugin.XRay/PluginServiceRegistrator.cs
+++ Jellyfin.Plugin.XRay/PluginServiceRegistrator.cs
@@ -1,5 +1,6 @@
 using Jellyfin.Plugin.XRay.Services;
 using MediaBrowser.Controller.Plugins;
+using MediaBrowser.Controller;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Jellyfin.Plugin.XRay;
@@ -10,7 +11,7 @@ namespace Jellyfin.Plugin.XRay;
 public class PluginServiceRegistrator : IPluginServiceRegistrator
 {
     /// <inheritdoc />
-    public void RegisterServices(IServiceCollection serviceCollection, IServiceProvider applicationServiceProvider)
+    public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
     {
         serviceCollection.AddSingleton<XRayStore>();
         serviceCollection.AddSingleton<SidecarClient>();
